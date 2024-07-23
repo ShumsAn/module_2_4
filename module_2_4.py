7894737bd94788384b5f33b37da85d9eb6e982d9
ShumsAn <shum-sol@outlook.com>
2024-07-23 16:23:51 +0300

    Задача "Всё не так уж просто"

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for n in numbers:
+    is_primes = True
+    for i in range(2, n):
+        if n % i == 0:
+            is_primes = False
+            break
+    if is_primes == False:
+        not_primes.append(n)
+    if is_primes == True and n != 1:
+        primes.append(n)
+
+print('Простые:', primes)
+print('НЕ Простые:', not_primes)
